9d03812292cc2356bac09b6f3e5cda050a00afea
AlexRax1 <alex123raxon@gmail.com>
2025-05-09 17:07:46 -0600

    iniciacion

---
+++ src/main/java/agricultor/trasporteservice/TrasporteServiceApplication.java
@@ -0,0 +1,13 @@
+package agricultor.trasporteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasporteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrasporteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/agricultor/trasporteservice/TrasporteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package agricultor.trasporteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasporteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
